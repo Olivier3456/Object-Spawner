28290dece6d6040d78d689a2e7cfddec76cd4078
Olivier3456 <olivier.larrey@gmail.com>
2023-01-06 15:37:52 +0100

    Limite au nombre d'objets instantiables

--- Assets/Scripts/SpawnCube.cs
+++ Assets/Scripts/SpawnCube.cs
@@ -9,6 +9,9 @@ public class SpawnCube : MonoBehaviour
 
     [SerializeField] private Camera _cam;
 
+    [SerializeField] private int _nombreMaxDObjets;
+    private int _nombreDObjets;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +21,16 @@ public class SpawnCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+
+
+        if (Input.GetMouseButtonDown(0) && _nombreDObjets < _nombreMaxDObjets)
         {
             Vector3 positionObjectToSpawn = _cam.ScreenToWorldPoint(
                 new Vector3(Input.mousePosition.x, Input.mousePosition.y, 25));
 
-            Instantiate(_prefabToSpawn, positionObjectToSpawn, Quaternion.identity);            
+            Instantiate(_prefabToSpawn, positionObjectToSpawn, Quaternion.identity);
+            _nombreDObjets++;
         }
+        else if (_nombreDObjets >= _nombreMaxDObjets) Debug.Log("Nombre maximum d'objets atteint :" + _nombreDObjets);
     }
 }
