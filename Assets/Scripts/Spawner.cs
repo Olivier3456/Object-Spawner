489cde7e5038aef3298f8da9c824b6779f86f986
Olivier3456 <olivier.larrey@gmail.com>
2023-01-09 10:10:39 +0100

    Minor changes on Random position of objects

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -34,49 +34,40 @@ public class Spawner : MonoBehaviour
         {        
             if (_objectsSpawned.Count > _nombreMaxDObjets)
             {
-                RelocateOldestObject(PositionToSpawn());
+                RelocateOldestObject();
             }
             else
             {
-                InstantiateNewObject(PositionToSpawn());
+                InstantiateNewObject();
             }
 
             _time = 0;
         }
     }
-
-    private Vector3 PositionToSpawn()
-    {
-        float xRandom = OffsetAleatoire();
-        float yRandom = OffsetAleatoire();
-        float zRandom = OffsetAleatoire();
-
-        Vector3 positionToSpawn = new Vector3(_centreDeLaZoneDeSpawn.position.x + xRandom,
-                                              _centreDeLaZoneDeSpawn.position.y + yRandom,
-                                              _centreDeLaZoneDeSpawn.position.z + zRandom);
-        return positionToSpawn;
-    }
-
-    private void RelocateOldestObject(Vector3 positionToSpawn)
+        
+    private void RelocateOldestObject()
     {
         _tempGameObject = _objectsSpawned[_nombreMaxDObjets];
-        _tempGameObject.transform.position = positionToSpawn;
+        _tempGameObject.transform.position = NewRandomPosition();
 
         _objectsSpawned.Remove(_objectsSpawned[_nombreMaxDObjets]);
 
         _objectsSpawned.Insert(0, _tempGameObject);
     }
 
-    private void InstantiateNewObject(Vector3 positionToSpawn)
+    private void InstantiateNewObject()
     {
-        _objectsSpawned.Insert(0, Instantiate(_prefabToSpawn, positionToSpawn, Quaternion.identity));
+        _objectsSpawned.Insert(0, Instantiate(_prefabToSpawn, NewRandomPosition(), Quaternion.identity));
 
         Color newColor = new Color(Random.Range(0, 1.0f), Random.Range(0, 1.0f), Random.Range(0, 1.0f));
         _objectsSpawned[0].GetComponent<Renderer>().material.color = newColor;
     }
 
-    private float OffsetAleatoire()
+
+    private Vector3 NewRandomPosition()
     {
-        return Random.Range(_rayonDeLaZoneDeSpawn, -_rayonDeLaZoneDeSpawn);
+        return Random.insideUnitSphere * _rayonDeLaZoneDeSpawn + _centreDeLaZoneDeSpawn.position;
     }
+
+
 }
